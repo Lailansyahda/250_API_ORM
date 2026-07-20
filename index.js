c37d35f18db9b80cb4883da8165859788653d81b
Lailansyahda <syahdaazalia123@gmail.com>
2026-07-20 14:54:17 +0700

    feat: sinkronisasi database sequelize sebelum menjalankan server

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ app.use(express.urlencoded({ extended: false }));
 app.listen(port, () => {
   console.log(`Server started on port 3000`);
 })
+
+db.sequelize.sync()
+    .then((result) => {
+        app.listen(3000, () => {
+            console.log('Server started');
+        });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
