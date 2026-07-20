6ceada2c599e863fd1c6c8c9491f48480a69f95f
Lailansyahda <syahdaazalia123@gmail.com>
2026-07-20 14:51:20 +0700

    feat: aktifkan listen port untuk server express

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const port = 3000;
 const db = require('./models');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.listen(port, () => {
+  console.log(`Server started on port 3000`);
+})
